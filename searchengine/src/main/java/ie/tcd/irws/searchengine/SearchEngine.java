e976bd939c70f78bd6f11799f0c378b4e88cf10a
Keith Tunstead <tunstek@tcd.ie>
2018-11-11 11:55:06 +0000

    Created buildIndex method in SearchEngine.java

--- searchengine/src/main/java/ie/tcd/irws/searchengine/SearchEngine.java
+++ searchengine/src/main/java/ie/tcd/irws/searchengine/SearchEngine.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 
 import ie.tcd.irws.searchengine.parsers.FBISParser;
 import ie.tcd.irws.searchengine.parsers.FRParser;
+import ie.tcd.irws.searchengine.parsers.LAParser;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
@@ -20,45 +21,53 @@ import ie.tcd.irws.searchengine.parsers.FTParser;
 public class SearchEngine 
 {
 	private static Directory indexDirectory;
-	
+
 	private static String FT_DIR = "corpus/ft";
 	private static String FR_DIR = "corpus/fr94";
-	private static String LA_DIR = "corpus/fr94";
-	private static String FBIS_DIR = "corpus/latimes";
+	private static String LA_DIR = "corpus/latimes";
+	private static String FBIS_DIR = "corpus/fbis";
 	private static String INDEX_DIR = "index";
 	
-    public static void main( String[] args ) throws IOException{
+    public static void main( String[] args ) throws IOException {
     	
     	Analyzer analyzer = new StandardAnalyzer();
     	indexDirectory = FSDirectory.open(Paths.get(INDEX_DIR));
-    	
-    	 // create and configure an index writer
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-        IndexWriter iwriter = new IndexWriter(indexDirectory, config);
-    	
-    	// FT
-    	FTParser ftp = new FTParser(getFilePaths(FT_DIR));
-    	ftp.loadDocs(iwriter);
 
-		// FR
-		FRParser frp = new FRParser(getFilePaths(FR_DIR));
-		frp.loadDocs(iwriter);
+    	buildIndex(analyzer);
+    }
+
+	/**
+	 *  Method for building the index
+	 *  @param Analyzer
+	 *  @return void
+	 *  @throws IOException
+	 */
+	private static void buildIndex(Analyzer analyzer) throws IOException {
+		// create and configure an index writer
+		IndexWriterConfig config = new IndexWriterConfig(analyzer);
+		config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+		IndexWriter iwriter = new IndexWriter(indexDirectory, config);
+
+		// FT
+		FTParser ftp = new FTParser(getFilePaths(FT_DIR));
+		ftp.loadDocs(iwriter);
+
+		// FR - Stops at file number 459? - seems to enter infinite loop in JSoup
+		//FRParser frp = new FRParser(getFilePaths(FR_DIR));
+		//frp.loadDocs(iwriter);
 
 		// LA
-		FRParser lap = new FRParser(getFilePaths(LA_DIR));
+		LAParser lap = new LAParser(getFilePaths(LA_DIR));
 		lap.loadDocs(iwriter);
 
 		// FBIS
 		FBISParser fbisp = new FBISParser(getFilePaths(FBIS_DIR));
 		fbisp.loadDocs(iwriter);
 
-    	
-    	iwriter.close();
-    }
+		iwriter.close();
+	}
 
-
-    /**
+	/**
      * Method to get all paths of files in a given directory
      * @param directory
      * @return String array of all file paths
