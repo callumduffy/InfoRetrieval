5a91e8ae4bf4b95def85bd9e14259865924d996c
Daniel Lavelle <lavelld@tcd.ie>
2018-11-16 14:38:19 +0000

    Adding notebook for analysis on individual queries

--- searchengine/run_search_engine.sh
+++ searchengine/run_search_engine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source config.cfg
-mvn clean compile package
-mvn exec:java -Dexec.mainClass="ie.tcd.irws.searchengine.SearchEngine"
+#mvn clean compile package
+#mvn exec:java -Dexec.mainClass="ie.tcd.irws.searchengine.SearchEngine"
 cd $trec_location
-./trec_eval $qrels_location $results_location
+./trec_eval -q $qrels_location $results_location | tee $trec_results_location 
